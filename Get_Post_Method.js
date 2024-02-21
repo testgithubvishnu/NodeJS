508267c860b5863c67f3edde2871fb2673bce36e
Manale Akshata Kamlakar <akshatamanale@gmail.com>
2024-02-21 08:50:53 +0530

    Todey's task

--- Get_Post_Method.js
+++ Get_Post_Method.js
@@ -17,17 +17,22 @@ http
 
     let parsedUrl = url.parse(req.url, true);
     console.log(parsedUrl);
+
+    // Reading the file as string:
     let products = fs.readFileSync("./products.json", "utf-8");
 
     //console.log(parsedUrl.query.id);
 
+    //fetch all the products:
     if (
       parsedUrl.pathname == "/products" &&
       req.method == "GET" &&
       parsedUrl.query.id == undefined
     ) {
       res.end(products);
-    } else if (
+    }
+    // Fetch product based on the id:
+    else if (
       parsedUrl.pathname == "/products" &&
       req.method == "GET" &&
       parsedUrl.query.id != undefined
@@ -43,18 +48,25 @@ http
       } else {
         res.end(JSON.stringify({ massage: "product not found" }));
       }
-    } else if (req.method == "POST" && parsedUrl.pathname == "/products") {
+    }
+
+    // Create a new product:
+    else if (req.method == "POST" && parsedUrl.pathname == "/products") {
       let product = "";
 
+      // This event is called for every chunk received:
       req.on("data", (chunk) => {
         console.log(chunk);
         product += chunk;
       });
       // console.log(req);
+
+      //This event id called at the end of stream and converts bytes to readable string:
       req.on("end", () => {
         //console.log(product);
         let productArray = JSON.parse(products);
         let newproduct = JSON.parse(product);
+
         productArray.push(newproduct);
         fs.writeFile("./products.json", JSON.stringify(productArray), (err) => {
           if ((err = null)) {
@@ -65,6 +77,19 @@ http
         });
       });
       res.end("post request working");
+    }
+    // end point to delete a product based on id:
+    else if (req.method == "DELETE" && parsedUrl.pathname == "/products") {
+      let productsArray = JSON.parse(product);
+
+      let index = productsArray.findIndex((product) => {
+        return product.id == parsedUrl.query.id;
+      });
+      productsArray.splice(index, 1);
+
+      // a=[23,75,94,69]
+      // a.splice(1,1) -> remove 75
+      // a.splice(1,2) -> remove 75,94
     }
   })
   .listen(7000);
