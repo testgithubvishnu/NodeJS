2caea119446d5702275afee171f9646d875519b4
Manale Akshata Kamlakar <121751451+testgithubvishnu@users.noreply.github.com>
2024-02-25 08:55:41 +0530

    Today's Task

--- DBWork/index.js
+++ DBWork/index.js
@@ -15,41 +15,47 @@ mongoose
 // });
 
 // Validating schema:
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is Manadatory"], // custom massage
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is Manadatory"], // custom massage
+    },
+    password: {
+      type: String,
+      maxLength: [12, "maximum is 12"],
+      minLength: [5, "minimum is 5"],
+    },
+    age: {
+      type: Number,
+      min: [10, "minimum age is 10"],
+      max: 100,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "manager", "customer"], // collection of constant
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    maxLength: [12, "maximum is 12"],
-    minLength: [5, "minimum is 5"],
-  },
-  age: {
-    type: Number,
-    min: [10, "minimum age is 10"],
-    max: 100,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ["admin", "manager", "customer"], // collection of constant
-    required: true,
-  },
-});
+  { timestamps: true } // Timestamp includes
+);
 
 // model: collection of schema
 // no. of collections = no. of schemas = no.of model
 const userModel = mongoose.model("users", userSchema);
 
 // Inserting data:
+//===============
 let user = {
-  name: "Aman gupta",
-  age: 42,
-  password: "gupta",
-  role: "admin",
+  name: "Vinit Singh",
+  age: 22,
+  password: "vsingh",
+  role: "manager",
 };
 
+// Insert data:
+//============
 userModel
   .create(user)
   .then((data) => {
@@ -58,3 +64,50 @@ userModel
   .catch((err) => {
     console.log(err);
   });
+
+// Find data:
+//===========
+// userModel
+//   .findOne({ name: "Aman gupta" })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+//Likewisw findMany()
+
+// sort data:
+//==========
+// userModel
+//   .find()
+//   .sort({ age: 1 })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// Delete function:
+//===============
+// userModel
+//   .deleteOne({ age: 55 })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+// likewise deleteMany()
+
+//Update function:
+//================
+// userModel
+//   .updateOne({ name: "Aman gupta" }, { name: "Anupam mittal" })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
