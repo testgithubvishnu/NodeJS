273333829bd4eb3011edf3fee3cfae25ca0c775e
Manale Akshata Kamlakar <121751451+testgithubvishnu@users.noreply.github.com>
2024-03-07 08:33:17 +0530

    Today's task

--- API-Express/index.js
+++ API-Express/index.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+// app.get(path , functionality)
+
 app.get("/products", (req, res) => {
   console.log("Get request coming");
   res.send({ message: "Get request success" });
@@ -11,6 +13,11 @@ app.get("/products", (req, res) => {
 app.get("/users/:id", (req, res) => {
   console.log(req.params.id);
   res.send({ message: "users details " });
+});
+
+app.get("/about", (req, res) => {
+  console.log("About us");
+  res.send({ message: "About us" });
 });
 
 app.get("/testing/:id", middleware, (req, res) => {
@@ -26,6 +33,7 @@ function middleware(req, res, next) {
   }
 }
 
+// start the server:
 app.listen(8000, () => {
   console.log("server is running on port 8000");
 });
